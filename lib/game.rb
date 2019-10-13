152e842c4cb2852faf9105dd2527df0a515ef874
Anders Carling <lowe@lowe.nu>
2019-10-13 23:11:04 +0200

    Implement code kata

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,76 @@
-# class Game
-# end
+class Game
+  SCORES = [0, 15, 30, 40]
+
+  def initialize
+    @player1_score = 0
+    @player2_score = 0
+  end
+
+  private def score_diff
+    @player1_score - @player2_score
+  end
+
+  private def is_deuce
+    @player1_score > 2 && score_diff == 0
+  end
+
+  private def is_early_game
+    !is_deuce &&
+      @player1_score < 4 && @player2_score < 4
+  end
+
+  private def is_advantage_p1
+    !is_early_game &&
+      score_diff == 1
+  end
+
+  private def is_advantage_p2
+    !is_early_game &&
+      score_diff == -1
+  end
+
+  private def is_winner_p1
+    !is_early_game &&
+      score_diff >= 2
+  end
+
+  private def is_winner_p2
+    !is_early_game &&
+      score_diff <= -2
+  end
+
+  def score
+    case
+    when is_deuce
+      "Deuce"
+    when is_early_game
+      "#{SCORES.fetch(@player1_score)} - #{SCORES.fetch(@player2_score)}"
+    when is_advantage_p1
+      "Advantage Player #1!"
+    when is_advantage_p2
+      "Advantage Player #2!"
+    when is_winner_p1
+      "A winner is Player #1!"
+    when is_winner_p2
+      "A winner is Player #2!"
+    end
+  end
+
+  private def assert_game_not_finished!
+    if is_winner_p1 || is_winner_p2
+      raise RuntimeError, "Game is finshed"
+    end
+  end
+
+  def award_point_to_player1
+    assert_game_not_finished!
+
+    @player1_score += 1
+  end
+
+  def award_point_to_player2
+    assert_game_not_finished!
+
+    @player2_score += 1
+  end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,5 +3,167 @@ require 'game'
 RSpec.describe Game do
   subject { Game.new }
 
-  pending "should be tested"
+  def award_points(player1: 0, player2: 0)
+    player1.times { subject.award_point_to_player1() }
+    player2.times { subject.award_point_to_player2() }
+  end
+
+  it "should have correct initial scores" do
+    expect(subject.score).to eq("0 - 0")
+  end
+
+  context "player 1 score" do
+
+    it "should be 15 after one point" do
+      award_points(player1: 1)
+
+      expect(subject.score).to eq("15 - 0")
+    end
+
+    it "should be 30 after two points" do
+      award_points(player1: 2)
+
+      expect(subject.score).to eq("30 - 0")
+    end
+
+    it "should be 40 after three points" do
+      award_points(player1: 3)
+
+      expect(subject.score).to eq("40 - 0")
+    end
+
+    it "should be winner after four consequtive points" do
+      award_points(player1: 4)
+
+      expect(subject.score).to eq("A winner is Player #1!")
+    end
+
+  end
+
+  context "player 2 score" do
+
+    it "should be 15 after one point" do
+      award_points(player2: 1)
+
+      expect(subject.score).to eq("0 - 15")
+    end
+
+    it "should be 30 after two points" do
+      award_points(player2: 2)
+
+      expect(subject.score).to eq("0 - 30")
+    end
+
+    it "should be 40 after three points" do
+      award_points(player2: 3)
+
+      expect(subject.score).to eq("0 - 40")
+    end
+
+    it "should be winner after four consequtive points" do
+      award_points(player2: 4)
+
+      expect(subject.score).to eq("A winner is Player #2!")
+    end
+
+  end
+
+  context "end game score" do
+
+    it "should be Deuce if tied at 40" do
+      award_points(player1: 3, player2: 3)
+
+      expect(subject.score).to eq("Deuce")
+    end
+
+    it "should be advantage player 1 if player 1 wins at deuce" do
+      award_points(player1: 3, player2: 3)
+      award_points(player1: 1)
+      expect(subject.score).to eq("Advantage Player #1!")
+    end
+
+    it "should be advantage player 2 if player 2 wins at deuce" do
+      award_points(player1: 3, player2: 3)
+      award_points(player2: 1)
+      expect(subject.score).to eq("Advantage Player #2!")
+    end
+
+    it "should be winner player 1 if player 1 wins two points at deuce" do
+      award_points(player1: 3, player2: 3)
+      award_points(player1: 2)
+      expect(subject.score).to eq("A winner is Player #1!")
+    end
+
+    it "should be winner player 2 if player 2 wins two points at deuce" do
+      award_points(player1: 3, player2: 3)
+      award_points(player2: 2)
+      expect(subject.score).to eq("A winner is Player #2!")
+    end
+
+    it "should be deuce if player 1 breaks advantage" do
+      award_points(player1: 3, player2: 3)
+      award_points(player2: 1)
+      award_points(player1: 1)
+
+      expect(subject.score).to eq("Deuce")
+    end
+
+    it "should be deuce if player 1 breaks advantage" do
+      award_points(player1: 3, player2: 3)
+      award_points(player1: 1)
+      award_points(player2: 1)
+
+      expect(subject.score).to eq("Deuce")
+    end
+
+    it "should be adv. player 1 if player 1 breaks advantage and scores point" do
+      award_points(player1: 3, player2: 3)
+      award_points(player2: 1)
+      award_points(player1: 2)
+
+      expect(subject.score).to eq("Advantage Player #1!")
+    end
+
+    it "should be adv. player 2 if player 2 breaks advantage and scores point" do
+      award_points(player1: 3, player2: 3)
+      award_points(player1: 1)
+      award_points(player2: 2)
+
+      expect(subject.score).to eq("Advantage Player #2!")
+    end
+
+    it "should be winner player 1 if player 1 wins 3 points at disadvantage" do
+      award_points(player1: 3, player2: 3)
+      award_points(player2: 1)
+      award_points(player1: 3)
+
+      expect(subject.score).to eq("A winner is Player #1!")
+    end
+
+    it "should be winner player 2 if player 2 wins 3 points at disadvantage" do
+      award_points(player1: 3, player2: 3)
+      award_points(player1: 1)
+      award_points(player2: 3)
+
+      expect(subject.score).to eq("A winner is Player #2!")
+    end
+
+  end
+
+  context "incorrect input" do
+
+    it "should not accept points after match is done" do
+      award_points(player1: 4)
+
+      expect {
+        subject.award_point_to_player1()
+      }.to raise_error(RuntimeError)
+
+      expect {
+        subject.award_point_to_player2()
+      }.to raise_error(RuntimeError)
+    end
+
+  end
+
 end
